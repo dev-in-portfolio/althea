6244cc8f499bddcd73ff4a4265cf11e552f04ee2
root <root@localhost.localdomain>
2026-02-27 18:16:14 +0000

    Remove build artifacts

--- .next/static/6YGiORdXlWPuo3XdlBI2p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-ec28eb720737a83a.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
